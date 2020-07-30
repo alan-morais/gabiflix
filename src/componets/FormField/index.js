577722884d7b3e47ab8dc75339cb48af06b3b6a6
Alan Morais <alan.franc@hotmail.com>
2020-07-30 20:40:44 -0300

    Eslint prop-types

--- src/componets/FormField/index.js
+++ src/componets/FormField/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
-function FormField({ label, type, name, value, onChange }) {
-    return (
-        <div>
-            <label>
-                {label}:
-                <input
-                    type={type}
-                    value={value}
-                    name={name}
-                    onChange={onChange}
-                />
-            </label>
-        </div>
-    )
-};
+function FormField({
+  label, type, name, value, onChange,
+}) {
+  return (
+    <div>
+      <label>
+        {label}
+        :
+        <input
+          type={type}
+          value={value}
+          name={name}
+          onChange={onChange}
+        />
+      </label>
+    </div>
+  );
+}
 
 export default FormField;
-//1:04:50 
