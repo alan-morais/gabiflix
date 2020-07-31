2d5577863765976860188af5792ed6cbce985d60
Alan Morais <alan.franc@hotmail.com>
2020-07-30 22:18:27 -0300

    css

--- src/componets/FormField/index.js
+++ src/componets/FormField/index.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-const FormFieldWrapper = styled.input`
-
+const FormFieldWrapper = styled.div`
+position: relative;
+textarea {
+  min-height: 150px;
+}
+input[type="color"] {
+  padding-left: 56px;
+}
 `;
 
-const Label = styled.label`
-
-`;
+const Label = styled.label``;
 
 const Input = styled.input`
-
+background: #53585D;
+  color: #F5F5F5;
+  display: block;
+  width: 100%;
+  height: 57px;
+  font-size: 18px;
+  
+  outline: 0;
+  border: 0;
+  border-top: 4px solid transparent;
+  border-bottom: 4px solid #53585D;
+  
+  padding: 16px 16px;
+  margin-bottom: 45px;
+  
+  resize: none;
+  border-radius: 4px;
+  transition: border-color .3s;
+  
+  &:focus {
+    border-bottom-color: var(--primary);
+  }
+  &:focus:not([type='color']) + ${Label.Text} {
+    transform: scale(.6) translateY(-10px);
+  }
+  ${({ value }) => {
+    const hasValue = value.length > 0;
+    return hasValue && css`
+        &:not([type='color']) + ${Label.Text} {
+          transform: scale(.6) translateY(-10px);
+        }
+      `;
+  }
+}
 `;
 function FormField({
   label, type, name, value, onChange,
